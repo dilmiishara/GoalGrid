0fde0043f97303b779efb0c665c4975ca37ee299
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-14 17:45:52 +0530

    login function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import { createError } from "../utils/error.js";
 import { connectToDB } from "../utils/connect.js";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function register(req, res, next) {
     const data = req.body;
@@ -16,18 +17,35 @@ export async function register(req, res, next) {
     const alreadyRegistered = await User.exists({ email: data.email });
     if (alreadyRegistered) return next(createError(400, "User already exists."));
 
-    // Fix: Use bcrypt.genSaltSync(10) instead of bcrypt.getSaltSync(10)
-    const salt = bcrypt.genSaltSync(10);  // Updated line
+    const salt = bcrypt.genSaltSync(10); 
     const hash = bcrypt.hashSync(req.body.password, salt);
-
     const newUser = new User({ ...req.body, password: hash });
-    await newUser.save();  // Save the new user to the database
+    await newUser.save(); 
 
-    res.status(201).json("User created successfully");  // Respond with the newly created user
+    res.status(201).json("User created successfully");  
 }
 
 export async function login(req, res, next) {
-    // Login logic goes here
+    const data = req.body;
+    console.log(data);
+
+    if (!data?.email || !data?.password) {
+        return next(createError(400, "Missing fields"));
+    }
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return next(createError(400,"Invalid credentials"));
+    const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        user.password
+    );
+    if(!isPasswordCorrect) return next(createError(400,"Invalid credentials"));
+    const token = jwt.sign({id:user._id},process.env.JWT);
+    console.log(token);
+    // res.send('login route!');
+    res.cookie("access_token", token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+    }).status(200).json("User logged in!")
 }
 
 export async function logout(req, res, next) {
