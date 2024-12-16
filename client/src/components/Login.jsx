be2de9d62959c82715cf664a1bf34dae38519ddc
dilmiishara2002 <dilmiishara2000@gmail.com>
2024-12-16 19:34:29 +0530

    register and login UI

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {Label} from "./ui/label";
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>
-      login page
-    </div>
-  )
-}
+  return(
+    <div className="h-screen flex justify-center items-center transform -translate-y-16">
+        <form action="" className='flex flex-col gap-6 max-w-xl w-full px-8'>
+            <div className='flex flex-col gap-2'>
+                <Label>Email</Label>
+                <Input type="email" name="email" placeholder="Enter email"/>
+            </div>
+            <div className="flex flex-col gap-2">
+                <Label>Password</Label>
+                <Input type="password" name="password" placeholder="Enter password"/>
+            </div>
+            <Button>Login</Button>
+            <span className='text-[#63657b] text-center'>
+                Don't have an account? {" "}
+                <Link to="/register" className="transition ease-in-out hover:cursor-pointer
+                hover:text-primary hover:underline">
+                    Register
+                </Link>
+            </span>
 
-export default Login
+        </form>
+    </div>
+  );
+};
+
+export default Login;
